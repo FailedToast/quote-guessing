b4f5d2380a5692b1a1f2f25da34ec63d22d57158
FailedToast <pixelwinter07@gmail.com>
2025-12-04 08:53:55 -1000

    Added link example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,21 @@ export default function HomePage() {
 		<main className="">
 			<h1 className="">Teto</h1>
 			<p>Filler text this is text that is meant to display text not to actually mean something</p>
+			<a className="" target="_blank" href="https://youtu.be/A3iJITlDtLI?si=vneVdD3wxtoMo2fM"
+			style={{color: '#87CEFA'}}>This is A Link to Youtube</a>
 		</main>
 	);
 }
+/*
+Link
+https://youtu.be/A3iJITlDtLI?si=vneVdD3wxtoMo2fM
+
+Embed
+https://www.youtube.com/embed/A3iJITlDtLI?si=Owig9eZwWw6x53G
+
+Embed
+https://www.youtube.com/embed/A3iJITlDtLI
+
+Video Url
+https://youtu.be/A3iJITlDtLI
+*/
